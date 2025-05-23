b1603e53c33cdc699e089f71484b527afeed9570
zzmae <zzzmaee@mail.ru>
2025-05-23 19:15:43 +0500

    feat: added companies

--- src/app/pages/catalog/pages/catalog/catalog.component.ts
+++ src/app/pages/catalog/pages/catalog/catalog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {CardComponent} from '../../../../shared/components/card/card.component';
 
 @Component({
@@ -11,5 +11,34 @@ import {CardComponent} from '../../../../shared/components/card/card.component';
   styleUrl: './catalog.component.scss'
 })
 export class CatalogComponent {
-
+  cards = [
+    {
+      promotion: 'Акция',
+      status: 'В наличии',
+      imageUrl: 'assets/img/dp-5.png',
+      title: 'Делитель проб  желобчатый ДП 5',
+      subtitle: 'Оборудование для дробления и измельчения'
+    },
+    {
+      promotion: 'Акция',
+      status: 'В наличии',
+      imageUrl: 'assets/img/dp-5.png',
+      title: 'ЛинтеЛ® АТХ–20',
+      subtitle: 'Оборудование для испытания нефтепродуктов'
+    },
+    {
+      promotion: 'Акция',
+      status: 'В наличии',
+      imageUrl: 'assets/img/dp-5.png',
+      title: 'ИНФРАСКАН-3150',
+      subtitle: 'Оборудование для экспресс-анализа'
+    },
+    {
+      promotion: 'Акция',
+      status: 'В наличии',
+      imageUrl: 'assets/img/dp-5.png',
+      title: 'Анализатор жидкости "Эксперт-001"',
+      subtitle: 'РН-метры'
+    }
+  ];
 }
