556b76c3c435628ee50df18d582f0f00041d69a9
DarynDyussembaev <daryndyussembaev03@gmail.com>
2025-05-23 20:16:27 +0500

    contacts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {MainComponent} from './pages/main/main.component';
 import {CatalogComponent} from './pages/catalog/pages/catalog/catalog.component';
 import {CompaniesComponent} from './pages/companies/pages/companies/companies.component';
+import {ContactsComponent} from './pages/contacts/contacts.component';
 
 export const routes: Routes = [
   { path: "", redirectTo: "Home", pathMatch: "full" },
@@ -16,5 +17,9 @@ export const routes: Routes = [
   {
     path: "Catalog",
     component: CatalogComponent,
+  },
+  {
+    path: "contacts",
+    component: ContactsComponent,
   }
 ];
