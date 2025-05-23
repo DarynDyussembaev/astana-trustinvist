254f8d3848fa94cf75241520115ec6f2db8908ba
DarynDyussembaev <daryndyussembaev03@gmail.com>
2025-05-23 19:17:42 +0500

    trans

--- src/app/pages/catalog/catalog.component.ts
+++ src/app/pages/catalog/catalog.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {CardComponent} from "../../shared/components/card/card.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-catalog',
-  imports: [],
+    imports: [
+        CardComponent,
+        NgForOf
+    ],
   templateUrl: './catalog.component.html',
   standalone: true,
   styleUrl: './catalog.component.scss'
 })
 export class CatalogComponent {
-
 }
