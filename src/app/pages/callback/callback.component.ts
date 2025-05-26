96ec027e585297dc29ac17d5902ed81591c99300
DarynDyussembaev <daryndyussembaev03@gmail.com>
2025-05-26 11:11:06 +0500

    callback

---
+++ src/app/pages/callback/callback.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-callback',
+  imports: [],
+  templateUrl: './callback.component.html',
+  standalone: true,
+  styleUrl: './callback.component.scss'
+})
+export class CallbackComponent {
+
+}
