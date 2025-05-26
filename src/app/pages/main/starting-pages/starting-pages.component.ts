9ddec66257291285d63e63d9b2d01a36a7ac7aa1
DarynDyussembaev <daryndyussembaev03@gmail.com>
2025-05-26 10:58:51 +0500

    callback

--- src/app/pages/main/starting-pages/starting-pages.component.ts
+++ src/app/pages/main/starting-pages/starting-pages.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-starting-pages',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './starting-pages.component.html',
   standalone: true,
   styleUrl: './starting-pages.component.scss'
