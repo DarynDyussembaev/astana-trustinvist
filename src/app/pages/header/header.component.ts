8656b1796faf78217b8df5694a58e3844c2b06c1
DarynDyussembaev <daryndyussembaev03@gmail.com>
2025-05-28 11:55:27 +0500

    mobile burger menu

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {RouterLink} from '@angular/router';
 
 @Component({
@@ -11,5 +11,37 @@ import {RouterLink} from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isMobileMenuOpen = false;
 
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    this.toggleBodyScroll();
+  }
+
+  closeMobileMenu(): void {
+    this.isMobileMenuOpen = false;
+    this.toggleBodyScroll();
+  }
+
+  private toggleBodyScroll(): void {
+    if (this.isMobileMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
+
+  @HostListener('keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent): void {
+    if (event.key === 'Escape') {
+      this.closeMobileMenu();
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(): void {
+    if (window.innerWidth > 999) {
+      this.closeMobileMenu();
+    }
+  }
 }
